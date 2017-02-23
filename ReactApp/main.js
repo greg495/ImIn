2104634d19b1a4ebfa57af92a2cb26fdc2481843
gpnoel <gpnoel@users.noreply.github.com>
2017-02-22 20:56:11 -0500

    Adding code for backend connection to SQL database (#3)
    * Adding mysql package to replace using php
    
    * Adding unfinished/polished code to connect to SQL database

--- ReactApp/main.js
+++ ReactApp/main.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import SQL from './connect_to_sql.jsx';
 
 // ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<SQL />, document.getElementById('sql'));
 
-(navigator.geolocation.getCurrentPosition(function (position) {
+/*(navigator.geolocation.getCurrentPosition(function (position) {
     var mapCanvas = document.getElementById("map");
     var mapOptions = {
         center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
         zoom: 10
     };
     var map = new google.maps.Map(mapCanvas, mapOptions);
-}))();
+}))();*/
+
