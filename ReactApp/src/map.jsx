4fa865960527ef7a4f66bf1199c89e253994f6b7
Graigor Pierre-Noel <gpnoel111@yahoo.com>
2017-03-25 00:43:30 -0400

    adding map.jsx for the fix for Google Maps in our react project

--- ReactApp/src/map.jsx
+++ ReactApp/src/map.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-class Map extends React.Component {
-   render() {
-      return (
-         <div>
-         </div>
-      );
-   }
+export class Container extends React.Component {
+    render() {
+        const style = {
+            width: '70%',
+            height: '100%'
+        }
+        if (!this.props.loaded) {
+            return <div>Loading...</div>
+        }
+        return (
+            <Map google={this.props.google} style={style}/>
+        );
+    }
 }
 
-export default Map;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyAsA8dWaWu4HzipdUVUTgWGm_8xF8lGiWA"
+})(Container);
