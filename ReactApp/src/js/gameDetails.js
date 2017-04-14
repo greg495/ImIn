5313603261d2c09f86d739ccabbd72bc8ac1c483
kwschmitt <k.schmitt@verizon.net>
2017-04-14 01:50:08 -0400

    Updated game detail form

--- ReactApp/src/js/gameDetails.js
+++ ReactApp/src/js/gameDetails.js
@@ -1,7 +1,11 @@
+// Hide the game details form on startup
+document.getElementById("detailContainer").style.display = 'none';
+
 /* Function to show game detail for selected game */
 function showGameDetails(gameID) {
 	// Clear the div
 	document.getElementById("detailContainer").innerHTML = "";
+	document.getElementById("detailContainer").style.display = 'block';
 
 	// return if marker is being added
 	if ( gameID == 'Marker' ) {
@@ -19,29 +23,60 @@ function showGameDetails(gameID) {
 		var linebreak = document.createElement('BR');
 
 		// Make a label for the sport
-		var sportLabel = document.createTextNode('Sport: ' + data[0].sport);
+		var sportLabel = document.createElement("sportLabel");
+		sportLabel.innerHTML = 'Sport: ';
+		sportLabel.className += "detailLabel";
+		var sport = document.createElement("sport");
+		sport.innerHTML = data[0].sport;
 		detailContainer.appendChild(sportLabel);
+		detailContainer.appendChild(sport);
 		detailContainer.appendChild(linebreak);
 
 		// Make a label for the creator
-		var creatorLabel = document.createTextNode('Creator: ' + data[0].creator);
+		var creatorLabel = document.createElement("creatorLabel");
+		creatorLabel.innerHTML = 'Creator: ';
+		creatorLabel.className += "detailLabel";
+		var creator = document.createElement("creator");
+		creator.innerHTML = data[0].creatorName;
 		detailContainer.appendChild(creatorLabel);
+		detailContainer.appendChild(creator);
 
 		// Make img for creator
-		var creatorImage = document.createElement("img");
-		creatorImage.setAttribute('src', 'https://graph.facebook.com/'+data[0].creatorID+'/picture')
+		// var creatorImage = document.createElement("img");
+		// creatorImage.setAttribute('src', 'https://graph.facebook.com/'+data[0].creatorID+'/picture')
 		//detailContainer.appendChild(creatorImage);
 		linebreak = document.createElement('BR');
 		detailContainer.appendChild(linebreak);
 
+		// Make a label for the date
+		var dateLabel = document.createElement("dateLabel");
+		dateLabel.innerHTML = 'Date: ';
+		dateLabel.className += "detailLabel";
+		var date = document.createElement("date");
+		date.innerHTML = data[0].date;
+		detailContainer.appendChild(dateLabel);
+		detailContainer.appendChild(date);
+		linebreak = document.createElement('BR');
+		detailContainer.appendChild(linebreak);
+
 		// Make a label for the time
-		var timeLabel = document.createTextNode('time: ' );
+		var timeLabel = document.createElement("timeLabel");
+		timeLabel.innerHTML = 'Time: ';
+		timeLabel.className += "detailLabel";
+		var time = document.createElement("time");
+		time.innerHTML = data[0].time;
 		detailContainer.appendChild(timeLabel);
+		detailContainer.appendChild(time);
 		linebreak = document.createElement('BR');
 		detailContainer.appendChild(linebreak);
 
 		// Make a label for the description
-		var description = document.createTextNode('description: ' + data[0].description);
+		var descriptionLabel = document.createElement("descriptionLabel");
+		descriptionLabel.innerHTML = 'Description: ';
+		descriptionLabel.className += "detailLabel";
+		var description = document.createElement("description");
+		description.innerHTML = data[0].description;
+		detailContainer.appendChild(descriptionLabel);
 		detailContainer.appendChild(description);
 		linebreak = document.createElement('BR');
 		detailContainer.appendChild(linebreak);
@@ -78,6 +113,8 @@ function addButton(gameID) {
 
 	    	// Add the button to the page
 	    	detailContainer.appendChild(button);
+			linebreak = document.createElement('BR');
+			detailContainer.appendChild(linebreak);
 
 	    	// Add the list of palyers
 	    	addPlayerList(gameID);
@@ -95,6 +132,7 @@ function addPlayerList(gameID) {
     	var userTable = document.createElement("table");
     	var firstRow = userTable.insertRow(0);
     	firstRow.innerHTML = "Players:";
+    	firstRow.className += "detailLabel";
 
     	// Add a row for every user
     	data.forEach(function(element) {
